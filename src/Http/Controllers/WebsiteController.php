fb60199e46bd1847626035b33039117eb8c03d21
IAndreaGiuseppe <info@andreagiuseppe.com>
2019-11-24 21:26:52 +0100

    add website controller

---
+++ src/Http/Controllers/WebsiteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Iag\Agws\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class WebsiteController extends Controller
+{
+    public function homepage()
+    {
+    }
+}
