eaaeacecc2b8bf6b2b9945711f2c9f42956f9ebf
IAndreaGiuseppe <info@andreagiuseppe.com>
2019-12-07 17:09:24 +0100

    add a title on skills section

--- resources/views/homepage/skills.blade.php
+++ resources/views/homepage/skills.blade.php
@@ -1,65 +1,67 @@
-<div id="skills" class="hidden mt-16 px-6 py-16 bg-black text-white">
+<div id="skills" class="hidden mt-16 px-6 py-16 bg-agp-dark text-white">
     <div class="container mx-auto">
-    <div>
-        <p class="mb-6 font-medium text-lg">{{ __( "Linguaggi" ) }}</p>
+        <p class="mb-6 font-bold text-lg uppercase text-agp-yellow">{{ __( "Skills" ) }}</p>
 
-        <div class="flex flex-wrap justify-around">
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Italiano" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Inglese" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Francese" ) }}</span>
+        <div>
+            <p class="mb-6 font-medium text-lg">{{ __( "Linguaggi" ) }}</p>
+
+            <div class="flex flex-wrap justify-around">
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Italiano" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Inglese" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Francese" ) }}</span>
+            </div>
+
+            <div class="flex flex-wrap justify-around">
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Html5" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Css3" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "ES16" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "PHP" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "MySql" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "UML" ) }}</span>
+            </div>
         </div>
 
-        <div class="flex flex-wrap justify-around">
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Html5" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Css3" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "ES16" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "PHP" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "MySql" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "UML" ) }}</span>
+        <div>
+            <p class="mb-6 font-medium text-lg">{{ __( "Frameworks" ) }}</p>
+
+            <div class="flex flex-wrap justify-around">
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Symfony" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Laravel" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "VueJS" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Jquery" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Tailwind" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Bootstrap" ) }}</span>
+            </div>
         </div>
-    </div>
 
-    <div>
-        <p class="mb-6 font-medium text-lg">{{ __( "Frameworks" ) }}</p>
+        <div>
+            <p class="mb-6 font-medium text-lg">{{ __( "Software" ) }}</p>
 
-        <div class="flex flex-wrap justify-around">
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Symfony" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Laravel" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "VueJS" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Jquery" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Tailwind" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Bootstrap" ) }}</span>
+            <div class="flex flex-wrap justify-around">
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Git" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "CLI" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Putty" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "VS Code" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Workbench" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Firefox/Chrome" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Thunderbird" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Office" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Adobe" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Filezilla" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Visual Paradigm" ) }}</span>
+            </div>
         </div>
-    </div>
 
-    <div>
-        <p class="mb-6 font-medium text-lg">{{ __( "Software" ) }}</p>
+        <div>
+            <p class="mb-6 font-medium text-lg">{{ __( "Servizi" ) }}</p>
 
-        <div class="flex flex-wrap justify-around">
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Git" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "CLI" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Putty" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "VS Code" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Workbench" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Firefox/Chrome" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Thunderbird" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Office" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Adobe" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Filezilla" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Visual Paradigm" ) }}</span>
+            <div class="flex flex-wrap justify-around">
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "AWS" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Github" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Stackoverflow" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Twitter" ) }}</span>
+                <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Google" ) }}</span>
+            </div>
         </div>
-    </div>
-
-    <div>
-        <p class="mb-6 font-medium text-lg">{{ __( "Servizi" ) }}</p>
-
-        <div class="flex flex-wrap justify-around">
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "AWS" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Github" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Stackoverflow" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Twitter" ) }}</span>
-            <span class="inline-block mb-4 mr-2 px-3 py-1 text-sm font-medium shadow rounded border border-b-4 border-gray-400 bg-gray-200 text-gray-800">{{ __( "Google" ) }}</span>
-        </div>
-    </div>
     </div>
 </div>
