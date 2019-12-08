516b1d02b82472169c985a282ccc44d66960e1e8
IAndreaGiuseppe <info@andreagiuseppe.com>
2019-12-08 16:36:34 +0100

    remove diary menu link

--- resources/views/homepage/menu.blade.php
+++ resources/views/homepage/menu.blade.php
@@ -2,12 +2,4 @@
     <a href="{{ route('website.services') }}" class="flex justify-center mr-8">
         {{ __( "Services" ) }}
     </a>
-<!--
-    <a href="" class="flex">
-        <span class="w-6 mr-2">
-            @include('agws::svg.diary')
-        </span>
-        {{ __( "Diary" ) }}
-    </a>
--->
 </div>
