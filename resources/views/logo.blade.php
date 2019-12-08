b726f245506eeb13dc87725cfedad9d6db5f83e5
IAndreaGiuseppe <info@andreagiuseppe.com>
2019-12-08 16:11:45 +0100

    update service page structure

--- resources/views/logo.blade.php
+++ resources/views/logo.blade.php
@@ -1,7 +1,7 @@
 <a href="{{ route('website.homepage') }}" class="block font-black text-2xl txt-dark-container">
     <div class="flex items-center">
         <span class="w-10 mr-2 txt-dark-container">
-            @include('svg.spacehead')
+            @include('agws::svg.spacehead')
 
         </span>
 
