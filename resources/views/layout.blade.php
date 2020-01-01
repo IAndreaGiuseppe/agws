7ab96d18ca15c531abb907ecbe03b23acfb5c43a
IAndreaGiuseppe <info@andreagiuseppe.com>
2020-01-01 18:48:19 +0100

    add analytics

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,6 +15,10 @@
         <!-- javascript -->
         @stack('scripts')
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136952063-1"></script>
+        <script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-136952063-1');</script>
+
         <title>{{ config('app.name') }}{{ isset($title) ? " | {$title}" : '' }}</title>
     </head>
     <body class="font-sans font-normal leading-normal">
