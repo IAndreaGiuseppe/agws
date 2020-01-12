76da721712bac1c632c982bfd89145b037a78b2d
IAndreaGiuseppe <info@andreagiuseppe.com>
2020-01-12 15:48:10 +0100

    correct homepage and service view on website controller

--- src/Http/Controllers/WebsiteController.php
+++ src/Http/Controllers/WebsiteController.php
@@ -8,11 +8,11 @@ class WebsiteController extends Controller
 {
     public function homepage()
     {
-        return view('agws::wrapper', ['page' => 'agws::homepage.page']);
+        return view('agws::homepage.page');
     }
 
     public function services()
     {
-        return view('agws::wrapper', ['page' => 'agws::services.page']);
+        return view('agws::services.page');
     }
 }
