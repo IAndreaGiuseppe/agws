fca58ffb77ada1bd17a0eebc5bde3dd94fc345db
IAndreaGiuseppe <info@andreagiuseppe.com>
2020-01-12 15:52:20 +0100

    minor fix

--- resources/views/homepage/page.blade.php
+++ resources/views/homepage/page.blade.php
@@ -10,5 +10,5 @@
 @endsection
 
 @push('scripts')
-        <script src="{{ asset('vendor/agws/toggler.js') }}" defer></script>
+    <script src="{{ asset('vendor/agws/toggler.js') }}" defer></script>
 @endpush
