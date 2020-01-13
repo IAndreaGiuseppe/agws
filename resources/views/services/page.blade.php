e5f5ef6ab7ea267174fd3f1c43e4a861595ef744
IAndreaGiuseppe <info@andreagiuseppe.com>
2020-01-13 17:29:11 +0100

    include metadata entry point on services page

--- resources/views/services/page.blade.php
+++ resources/views/services/page.blade.php
@@ -6,3 +6,9 @@
     @include('agws::services.content')
 
 @endsection
+
+@push('metadata')
+    @include('metadata::metadata', [
+        'description' => 'I servizi di consulenza, hosting e sviluppo che offro per le aziende.'
+    ])
+@endpush
