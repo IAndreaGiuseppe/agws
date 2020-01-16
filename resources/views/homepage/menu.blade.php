381954bd5118218287f9fe806a2301d400823d89
IAndreaGiuseppe <info@andreagiuseppe.com>
2020-01-16 17:15:41 +0100

    add cv link on homepage

--- resources/views/homepage/menu.blade.php
+++ resources/views/homepage/menu.blade.php
@@ -1,5 +1,9 @@
 <div class="py-6 flex justify-center">
-    <a href="{{ route('website.services') }}" class="flex justify-center mr-8">
+    <a href="{{ route('website.cv') }}" class="flex justify-center mr-6">
+        {{ __( "CV" ) }}
+    </a>
+
+    <a href="{{ route('website.services') }}" class="flex justify-center">
         {{ __( "Services" ) }}
     </a>
 </div>
