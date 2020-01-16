a81325659cdcd0d2b272d8b6fd98e2d8363c3ff3
IAndreaGiuseppe <info@andreagiuseppe.com>
2020-01-16 15:35:40 +0100

    add basic cv views

---
+++ resources/views/cv/skills.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <p class="mb-4 px-8 font-semibold text-2xl">{{ __( "Computer Skills" ) }}</p>
+
+    <div>
+        <p>{{ __( "Linguaggi" ) }}</p>
+        <p>{{ __( "Html5, Css3, PHP, Javascript (ECMA 2016), MySql, UML" ) }}</p>
+
+        <p>{{ __( "Frameworks" ) }}</p>
+        <p>{{ __( "Symfony, Laravel, VueJS, Jquery, Tailwind, Bootstrap" ) }}</p>
+
+        <p>{{ __( "Software" ) }}</p>
+        <p>{{ __( "Git, CLI, Putty, VS Code, Workbench, Firefox/Chrome, Thunderbird, Office, Adobe, Filezilla, Visual Paradigm" ) }}</p>
+
+        <p>{{ __( "Services" ) }}</p>
+        <p>{{ __( "AWS, Github, Stackoverflow, Twitter, Google" ) }}</p>
+    </div>
+</div>
---
+++ resources/views/cv/summary.blade.php
@@ -0,0 +1,21 @@
+<div>
+    <p class="mb-4 px-8 font-semibold text-2xl">{{ __( "Sommario" ) }}</p>
+
+    <div>
+        <p>{{ __( "Più di 10 anni di esperienza nello sviluppo di applicazioni per il web. Titolare di partita iva dal 2008 attivamente dedicato allo sviluppo software ed integrazioni per aziende del nord italia." ) }}</p>
+        <p>{{ __( "Preciso nella scrittura del codice, curioso e sempre aggiornato" ) }}</p>
+        <p>{{ __( "Full-stack developer in grado di gestire le attività di deployment dalla scelta dell'infrastruttura fino alla realizzazione ultima del prodotto." ) }}</p>
+    </div>
+
+    <div>
+        <p>{{ __( "I miei strumenti di lavoro" ) }}</p>
+        <ul>
+            <li>Infrastruttura AWS</li>
+            <li>Framework back-end Laravel 6.* con sviluppo moduli isolati</li>
+            <li>Front-end Html5 + Scss, Tailwindcss + VueJS</li>
+            <li>Elementi grafici in Illustrator in formato .svg, .png, .jpeg</li>
+            <li>Versioning tramite GIT + Github</li>
+            <li>Inoltre: cli, bash, composer, npm, vscode, workbench</li>
+        </ul>
+    </div>
+</div>
